a2c5b0451515b3eb20f17717541e8a797cb57754
meshackkirwachumba <meshackkirwachumba@gmail.com>
2026-02-03 11:39:51 +0300

    insert new todo into database

---
+++ internal/repository/todo_repository.go
@@ -0,0 +1,33 @@
+package repository
+
+import (
+	"context"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/meshackkirwachumba/golang-postgres-todos/internal/models"
+)
+
+func CreateTodo(pool *pgxpool.Pool, title string, completed bool) (*models.Todo, error) {
+  var ctx context.Context 
+  var cancel context.CancelFunc
+  ctx, cancel = context.WithTimeout(context.Background(), 5 * time.Second)
+  defer cancel()
+
+  var query string = `
+      INSERT INTO todos_table (title, completed)
+	  VALUES ($1, $2)
+	  RETURNING id, title, completed, created_at, updated_at;
+  `
+
+  var todoRow models.Todo
+
+  err :=pool.QueryRow(ctx, query, title, completed).Scan(&todoRow.ID,&todoRow.Title,&todoRow.Completed,&todoRow.CreatedAt,&todoRow.UpdatedAt,
+  )
+
+  if err != nil {
+    return nil, err
+  }
+
+  return &todoRow, nil
+}	
